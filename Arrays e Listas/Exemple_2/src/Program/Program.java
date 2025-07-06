67d4094d8f79ddc5191124f08961191ef12e6544
Patrik Israel <patrik-israel@hotmai.com>
2025-07-05 22:02:09 -0300

    2nd exemple

---
+++ Arrays e Listas/Exemple_2/src/Program/Program.java
@@ -0,0 +1,35 @@
+package Program;
+
+import Entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < vect.length; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+        for (int i=0; i< vect.length; i++){
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+
+        System.out.printf("Avarage price = %.2f%n", avg);
+        sc.close();
+    }
+}
