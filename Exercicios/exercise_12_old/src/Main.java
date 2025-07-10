e669e25f726c4bb0bb26e78d2bdc35209cb8bad1
Patrik Israel <patrik-israel@hotmai.com>
2025-07-09 22:58:58 -0300

    update of exercise

--- Exercicios/exercise_12_old/src/Main.java
+++ Exercicios/exercise_12_old/src/Main.java
@@ -3,27 +3,34 @@ import java.util.Scanner;
 //Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Os nomes
 //devem ser armazenados em um vetor, e as idades em um outro vetor. Depois, mostrar na tela o nome
 //da pessoa mais velha.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("How many people will you register: ");
-        int n = sc.nextInt();
-        String [] vectName = new String[n];
-        double [] vectAge = new double[n];
-        String [] vectOlder= new String[n];
+        System.out.print("How many people will you register: ");
+        int n = Integer.parseInt(sc.nextLine());
 
-        for (int i= 0; i < vectName.length; i++){
-            System.out.println("Person " +(i + 1) +"data");
+        String[] names = new String[n];
+        int[] ages = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Person " + (i + 1) + " data");
             System.out.print("Name: ");
-            vectName[i] = sc.nextLine();
+            names[i] = sc.nextLine();
             System.out.print("Age: ");
-            vectAge[i] = sc.nextDouble();
+            ages[i] = Integer.parseInt(sc.nextLine());
         }
-        for (int i = 0)
-        System.out.println("Older Person: ");
-        sc.close();
 
+        int olderIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (ages[i] > ages[olderIndex]) {
+                olderIndex = i;
+            }
+        }
+
+        System.out.println("Older person: " + names[olderIndex]);
+        sc.close();
     }
 }
